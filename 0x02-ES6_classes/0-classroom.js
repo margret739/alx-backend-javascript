ed07710a413109b509609fe5192ac8c6293f3429
margret739 <margret739>
2024-10-08 18:55:31 +0000

    classjs

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,13 @@
+/**
+ * Represents a class room.
+ */
+export default class ClassRoom {
+	/**
+	 * Crates a new @see {@link ClassRoom}.
+	 * @param {Number} maxStudentsSize - The maximum number of
+	 * students in the class.
+	 */
+	constructor(maxStudentsSize) {
+	  this._maxStudentsSize = maxStudentsSize;
+	}
+}
