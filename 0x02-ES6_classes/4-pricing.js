34cf7f59e69dfac49bc01cd1ad4ae0c1e9f3405e
margret739 <margret739>
2024-10-09 10:00:09 +0000

    pricingjs

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,9 +1,9 @@
-import Currency from './3-currecncy';
+import Currency from './3-currency';
 
 export default class Pricing {
   constructor(amount, currency) {
-	  this.amount = amount;
-	  this.currency = currency;
+    this.amount = amount;
+    this.currency = currency;
   }
 
   get amount() {
