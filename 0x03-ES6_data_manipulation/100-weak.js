8701e500cd1315226e8d39b08f06402233bc5c7d
margret739 <margretandalya@gmail.com>
2024-10-20 21:16:47 +0000

    weakjs

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,28 @@
+/**
+ * weak map of endpoints and number of calls
+ */
+export const weakMap = new WeakMap();
+
+
+/**
+ * The maximum number of calls for an endpoint.
+ */
+const MAX_ENDPOINT_CALLS = 5;
+
+
+/**
+ * Tracks calls made to an API'S endpoint
+ * @param {{
+ *   protocal: String,
+ *   name: String,
+ * }} endpoint - the endpoint to make a request to.
+ */
+export function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
+  }
+  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+  if (weakMap.get(endpoint) >= MAX_ENDPOINT_CALLS) {
+    throw new Error('Endpoint load is high');
+  }
+}
