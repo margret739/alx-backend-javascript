30f1b56b7ca0855ced5c27de55314c37952931f6
margret739 <margretandalya@gmail.com>
2024-10-20 17:22:37 +0000

    data tsk

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,26 @@
+/**
+ * Updates the grades of a list of students in a given city.
+ * @param {{
+ *   id: Number,
+ *   firstName: String,
+ *   location: String
+ * }[]} newGrades - The new gades to be given to a students
+ * @returns {{id: Number, firstName: String, location: String}[]}
+ */
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  const defaultGrade = { grade: 'N/A' };
+  
+  if (students instanceof Array) {
+    return students
+      .filter((student) => student.location === city)
+      .map((student) => ({
+	id: student.id,
+	firstName: student.firstName,
+	location: student.location,
+	grade: (newGrades
+	  .filter((grade) => grade.studentId === student.id)
+	  .pop() || defaaultGrade).grade,
+      }));
+  }
+  return[];
+}
