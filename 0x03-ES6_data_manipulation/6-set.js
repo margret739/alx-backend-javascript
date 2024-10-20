5e6ee36041eb9a530828b30e71abfac1c0a80cc4
margret739 <margretandalya@gmail.com>
2024-10-20 20:07:03 +0000

    setjs

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The sorce array.
+ * @author Bezaleel Olakunori <https"//github.com/B3zaleel>
+ * @returns {set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
