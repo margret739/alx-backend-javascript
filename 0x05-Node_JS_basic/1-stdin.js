60ffea530e6d8d6140d5bc534078eff7ef0f4696
margret739 <margretandalya@gmail.com>
2024-10-22 14:32:49 +0000

    stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,6 +1,5 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-
 process.stdin.on('readable', () => {
   const chunk = process.stdin.read();
 
@@ -9,7 +8,6 @@ process.stdin.on('readable', () => {
     process.stdout.write(`Your name is: ${chunk}`);
   }
 });
-
 
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
