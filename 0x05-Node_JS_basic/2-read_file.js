620092eb16ca6910bd29880cffe11a1cad1ca6f8
margret739 <margretandalya@gmail.com>
2024-10-22 15:08:13 +0000

    fix

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -21,15 +21,15 @@ const countStudents = (dataPath) => {
   const studentPropNames = dbFieldNames.slice(0, dbFieldNames.length - 1);
 
   for (const line of fileLines.slice(1)) {
-     const studentRecord = line.split(',');
-     const studentPropValues = studentRecord.slice(0, studentRecord.length - 1);
-     const field = studentRecord[studentRecord.length - 1];
-     if (!Object.keys(studentGroups).includes(field)) {
-       studentGroups[field] = [];
-     }
-     const studentEntries = studentPropNames
-       .map((propName, idx) => [propName, studentPropValues[idx]]);
-     studentGroups[field].push(Object.fromEntries(studentEntries));
+    const studentRecord = line.split(',');
+    const studentPropValues = studentRecord.slice(0, studentRecord.length - 1);
+    const field = studentRecord[studentRecord.length - 1];
+    if (!Object.keys(studentGroups).includes(field)) {
+      studentGroups[field] = [];
+    }
+    const studentEntries = studentPropNames
+      .map((propName, idx) => [propName, studentPropValues[idx]]);
+    studentGroups[field].push(Object.fromEntries(studentEntries));
   }
 
   const totalStudents = Object
